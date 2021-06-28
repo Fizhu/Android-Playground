cff40093b0e4dd7480771a68996aa91877c12c38
fizhu <hvyz.anbiya@gmail.com>
2021-06-28 10:17:40 +0700

    adding webview

---
+++ app/src/main/java/com/fizhu/androidplayground/features/webview/WebviewActivity.kt
@@ -0,0 +1,106 @@
+package com.fizhu.androidplayground.features.webview
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import com.fizhu.androidplayground.databinding.ActivityWebviewBinding
+
+/**
+ * Created by fizhu on 28 June 2021
+ * https://github.com/Fizhu
+ */
+class WebviewActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebviewBinding
+    private var uploadMessage: ValueCallback<Uri>? = null
+    private var uploadMessageAboveL: ValueCallback<Array<Uri>>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbar.setNavigationOnClickListener { finish() }
+        initWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        binding.webview.settings.useWideViewPort = true
+        binding.webview.settings.loadWithOverviewMode = true
+        binding.webview.settings.javaScriptEnabled = true
+        binding.webview.settings.allowFileAccess = true
+        binding.webview.settings.allowContentAccess = true
+        binding.webview.settings.domStorageEnabled = true
+        binding.webview.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                binding.progressBar.progress = newProgress
+                binding.progressBar.visibility = if (newProgress == 100) View.GONE else View.VISIBLE
+            }
+
+            override fun onShowFileChooser(
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                uploadMessageAboveL = filePathCallback
+                openImageChooserActivity()
+                return true
+            }
+        }
+        binding.webview.loadUrl("https://google.com")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+            if (null == uploadMessage && null == uploadMessageAboveL) return
+            val result = if (data == null || resultCode != Activity.RESULT_OK) null else data.data
+            if (uploadMessageAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data)
+            } else if (uploadMessage != null) {
+                uploadMessage?.onReceiveValue(result)
+                uploadMessage = null
+            }
+        }
+    }
+
+    private fun onActivityResultAboveL(requestCode: Int, resultCode: Int, intent: Intent?) {
+        if (requestCode != FILE_CHOOSER_RESULT_CODE || uploadMessageAboveL == null)
+            return
+        var results: Array<Uri>? = null
+        if (resultCode == Activity.RESULT_OK) {
+            if (intent != null) {
+                val dataString = intent.dataString
+                val clipData = intent.clipData
+                if (clipData != null) {
+                    results = Array(clipData.itemCount) { i ->
+                        clipData.getItemAt(i).uri
+                    }
+                }
+                if (dataString != null)
+                    results = arrayOf(Uri.parse(dataString))
+            }
+        }
+        uploadMessageAboveL?.onReceiveValue(results)
+        uploadMessageAboveL = null
+    }
+
+    private fun openImageChooserActivity() {
+        val i = Intent(Intent.ACTION_GET_CONTENT)
+        i.addCategory(Intent.CATEGORY_OPENABLE)
+        i.type = "*/*"
+        startActivityForResult(Intent.createChooser(i, "File Chooser"), FILE_CHOOSER_RESULT_CODE)
+    }
+
+    companion object {
+        private const val FILE_CHOOSER_RESULT_CODE = 10000
+    }
+}
